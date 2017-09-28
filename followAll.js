006cae946cc215c283fdf153393630bafd583448
Dalin Huang <dhuan023@gmail.com>
2017-09-27 22:09:02 -0400

    Update followAll.js

--- followAll.js
+++ followAll.js
@@ -8,20 +8,33 @@
 // SOLUTION directly update URL to goto next page
 
 $(document).ready(function() {
-	//ex: "?page=100&tab=following"
-	var urlPara = location.search;
-  	var currentPage = parseInt(urlPara.match(/-?\d+\.?\d*/)[0]);
-  	//remove all unfollow form
-	$('.unfollow').remove();
+  //page 1
+  var bt_disabled = $('.pagination span').text() === "Previous";
+  //last page, not prev/next btn
+  var pre_next_btn = $('.pagination a').length;
+
+  //ex: "?page=100&tab=following"
+  var urlPara = location.search;
   
-  	$('.follow button').each(function(index, value) {
-  	    	let _this = $(this);
-		setTimeout(function() {
-			_this.trigger('click');
-		}, index*40);
-    	});
-	setTimeout(function() {
-		window.location = window.location.pathname + location.search.replace(currentPage, currentPage+1)
-	}, 3500);
+  var currentPage = location.search.match(/-?\d+\.?\d*/) ? parseInt(location.search.match(/-?\d+\.?\d*/)[0]) : 0;
+  //remove all unfollow form
+  $('.unfollow').remove();
+
+  $('.follow button').each(function(index, value) {
+    let _this = $(this);
+    setTimeout(function() {
+      _this.trigger('click');
+    }, index * 40);
+  });
+  setTimeout(function() {
+    if (bt_disabled) {
+      $('.pagination a').click();
+    } else if (pre_next_btn === 0) {
+      alert('DONE!');
+    } else {
+      window.location = window.location.pathname + location.search.replace(currentPage, currentPage + 1);
+    }
+  }, 3500);
 });
+
 
